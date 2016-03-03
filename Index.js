a25b48ecad6db371971f1e5a1821c4f2a2c43449
Anna Bezrukova <annelia_55@mail.ru>
2016-03-03 13:13:51 +0200

    clever circle

--- Index.js
+++ Index.js
@@ -1,3 +1,21 @@
 $(function() {
     $( ".circle_1" ).draggable();
 });
+$(document).ready(function() {
+    $(".circle_1").mouseenter(function() {
+        $(".circle_1").css("background-color", "yellow");
+        $( "p:first", this ).text("Yeeeee ^_^");
+        $(this).animate({
+            height   : "200",
+            width    : "200",
+        }, "fast");
+    });
+    $(".circle_1").mouseleave(function(){
+        $(".circle_1").css("background-color", "#61d258");
+        $( "p:first", this ).text("Thank you!");
+        $(this).animate({
+            height   : "150",
+            width    : "150",
+        }, "fast");
+    });
+});
