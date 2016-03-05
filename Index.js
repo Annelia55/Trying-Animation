bdefe692d90efd769a1b135ae8efffacfc0ac955
Anna Bezrukova <annelia_55@mail.ru>
2016-03-05 16:04:35 +0200

    add img

--- Index.js
+++ Index.js
@@ -1,21 +1,34 @@
-$(function() {
-    $( ".circle_1" ).draggable();
-});
+/* circle_1 */
 $(document).ready(function() {
     $(".circle_1").mouseenter(function() {
         $(".circle_1").css("background-color", "yellow");
         $( "p:first", this ).text("Yeeeee ^_^");
         $(this).animate({
-            height   : "200",
-            width    : "200",
+            height   : "250",
+            width    : "250",
         }, "fast");
     });
     $(".circle_1").mouseleave(function(){
-        $(".circle_1").css("background-color", "#61d258");
-        $( "p:first", this ).text("Thank you!");
+        $(".circle_1").css("background-color", "#67f45e");
+        $( "p:first", this ).text("Touch me and I'll grow");
         $(this).animate({
-            height   : "150",
-            width    : "150",
+            height   : "100",
+            width    : "100",
         }, "fast");
+    });
+});
+
+/* smile-drag */
+$(function() {
+    $( ".drag" ).draggable();
+});
+$(document).ready(function() {
+    $(".drag").mouseenter(function() {
+        $( "p:first", this ).text("Wohoooo");
+
+    });
+    $(".drag").mouseleave(function(){
+        $( "p:first", this ).text("Thanks");
+
     });
 });
